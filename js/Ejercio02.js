c985a81854b8f83c17221b7d7f7ede5f2e07e457
Brian jesus mendoza marquez <230308@utxicotepec.edu.mx.>
2024-09-20 14:27:36 -0600

    Practica04
    Agregacion de ejercio2 tipos de datos null y undefined

--- js/Ejercio02.js
+++ js/Ejercio02.js
@@ -1,104 +1,180 @@
+//Tipos de Datos en JS
 
-
-
-
-console.warn("tipo de dato UNDEFIEND")
+// 1.Undefined - (NUll)
+console.warn("--- Tipo de Dato UNDEFINED");
 let cliente;
-console.log(`el cliente es:  ${cliente}`);
-console.log(`el tipo de dato de la variable cliente es: ${typeof(cliente)}`);
+console.log(`El cliente es ${cliente}`);
+console.log(`El tipo de dato de la variable cliente es :${typeof (cliente)}`);
 
-cliente ="brian "
-console.log(`el cliente es : ${cliente}`);
-console.log(`el tipo de dato de la variable cliente es : ${typeof(cliente)}`);
+cliente = "Juan Romero";
+console.log(`El cliente es ${cliente}`);
+console.log(`El tipo de dato de la variable cliente es :${typeof (cliente)}`);
 
-cliente =19227
-console.log (`El cliente es : ${cliente}`);
-console.log(`el tipo de dato de la variable cliente es :${typeof(cliente)}`);
+cliente = 19227;
+console.log(`El cliente es ${cliente}`);
+console.log(`El tipo de dato de la variable cliente es :${typeof (cliente)}`);
 
-//2.Boleano -boleanos - true/false(falso / verdadero)
-console.warn("---Tipo de Dato:Bolean(boleano - true/false)")
-let esPremium="No lo se";
-console.log(`Es un cliente Premium?: ${esPremium}`)
-console.log(`El tipo de la variable esPremium es : ${typeof(esPremium)}`);
+//console.log (`El cliente ha realizado ${NumCompras} este mes`);
+//Undefined es el tipo de dato que por defecto se asigna a variables no inicializadas o no declaradas
 
-esPremium=false;
-console.log(`Es un cliente premiun ${esPremium}`);
-console.log(`El tipo de dato de la vaible es esPremium es : ${typeof(esPremium)}`);
-console.log(`Es un cliente premiun ${esPremium}`);
-if(esPremium)
-    console.log("es un cliente pago por usar el servicio");
-else
-console.log("Es un usuario premiun que recibe los sercios de gratuitos");
+//2.BOOLEAN - Boleanos- TRUE/FALSE (Falso/Verdadero)
+console.warn("---Tipo de Dato: BOOLEAN (Boleano- True/False)");
+let esPremium = "No lo se";
+console.log(`¿El cliente es Premium? ${esPremium}`);
+console.log(`El tipo de dato de la variable esPremium es :${typeof (esPremium)}`);
 
+console.log("Asignando el valor true a la variable")
+esPremium = "ture";
+console.log(`¿El cliente es Premium? ${esPremium}`);
+console.log(`El tipo de dato de la variable esPremium es :${typeof (esPremium)}`);
+
+console.log("Asignando el valor true a la variable")
+esPremium = true;
+console.log(`¿El cliente es Premium? ${esPremium}`);
+console.log(`El tipo de dato de la variable esPremium es :${typeof (esPremium)}`);
+
+console.log("Asignando el valor false a la variable")
+esPremium = 0;
+if (esPremium) {
+    console.log("El cliente pago por usar los servicios");
+} else {
+    console.log("El cliente recibe los servicios gratuitos");
+}
+
+//3. NUMBER (Números)
 var cantidad;
-const costo_producto=80.50
-let saldo_cuenta= -2500.40
+const costo_producto = 10.50;
+let saldo_cuenta = -2500.40;
+let monto_transaccion;
 
-console.warn("---Tipo de Dato - Numbre (Numeros , Positivos , negativos,decimales,flotantes");
-console.log(`tu saldo al de hoy es de: ${saldo_cuenta},(tipo de DAto =${typeof(saldo_cuenta)})`);
-console.log(`El producto que has seleccionado cuesta : ${costo_producto}`)
-cantidad =10 ;
-console.log(`Has elegido comprar: ${cantidad}de producto.`)
+console.warn("--- Tipo de Dato - NUMBER (Números,positivos,negativos,decimales,flotantes)");
+console.log(`Tu saldo al día de hoy es de :${saldo_cuenta}, (Tipo de Dato ${typeof (saldo_cuenta)})`);
+cantidad = 8;
+console.log(`Has elegido comprar:${cantidad} productos`);
+console.log(`El importe total de la compra es: ${cantidad * costo_producto}`);
+saldo_cuenta = saldo_cuenta - (cantidad * costo_producto);
+console.log(`Tu saldo nuevo es de ${saldo_cuenta}`);
+//El cliente realiza un bono de 1500
+monto_transaccion = 1500;
+console.log(`Tu monte de ${monto_transaccion} ha sido recibido, tu nuevo saldo es de: ${monto_transaccion + saldo_cuenta}`);
 
-console.log(`El importante total de la compra es ${(cantidad*costo_producto)}`)
-saldo_cuenta=saldo_cuenta - (cantidad*costo_producto)
-console.log(`Tu nuevo saldo es de : ${saldo_cuenta}`);
- //El cliente realiza un abono de 1500
- monto_transaccion =1500;
- console.log(`Tu abono de : ${monto_transaccion}ha recibido, tu nuevo saldo es de ${saldo_cuenta+monto_transaccion}`);
+//STRING (Cadena de Caracteres)
+const alumno = "Yáred Amaury Romero";
+let producto;
 
- // 4:String (cadena Caracteres)
- const alumno ="Brian jesus mendooza marquez"
- let producto;
- console.warn("---Tipo de Dato - String (Cadena de caracteres");
- console.log(`el Nombre del alumno es : ${alumno}, que es un tipo de dato ${typeof(alumno)}`);
+console.warn("---Tipo de dato - STRING (Cadena de Caracteres)");
+console.log(`El nombre del alumno es: ${alumno}, que es de tipo de dato ${typeof (alumno)}`);
 
- // Inicializamos el valor de la variable producto
- producto ="Monitor 20\"FULL HD"
- console.log(`El nombre del Producto es: ${producto},que es un tipo de dato${typeof (producto)}
- `);
+//Inicializamos el valor de la  variable producto
+producto = "MONITOR 20\"FULL HD";
+console.log(`El nombre del PRODUCTO es ${producto}, que es un tipo de dato ${typeof (producto)}`);
 
- // Manupalacion los String
- console.log(`Mas adelante podremos transformar el contenido de lo String usando los metodos y funciones especificaciones , como convertir su valor a mayusculas :
- ${alumno}=>${alumno.toLowerCase()}`);
- console.log(`O ensu defecto a minusculas: ${producto.toLowerCase()}`);
-
- // 5. BIGIN (Numero de Mayor Aplitud)
-
- console.warn("---Tipo de dato = Bigint (NUMERO AMPLIO");
- const numeroGRANde = 1234567890
- const numeroGRANde2 = 12345678901234567890
- 
-
- console.log(`El primero experimento de un numero grande es ${numeroGRANde},que si es soportado por NUmber y su tipo de dato es :
- ${typeof(numeroGRANde)}`);
-
- console.log(`El segundo experimento de un numero grande es ${numeroGRANde2},que si es soportado por NUmber y su tipo de dato es :
- ${typeof(numeroGRANde2)}`);
- console.log(`El tercer experimento de un numero grande es ${numeroGRANde3},que No es soportado por NUmber,perdiendo preción y su tipo de dato es :
- ${typeof(numeroGRANde3)}`);
- console.log(`El cuarto experimento de un numero grande es ${numeroGRANde4},que NO es soportado por NUmber,perdioendo precion y su tipo de dato es :
- ${typeof(numeroGRANde4)}`);
-
- numeroGRANde4=BigInt(1234567890123456789012345678901234567890);
- console.log(`El cuarto experimento de un numero grande es ${numeroGRANde4},que NO es soportado por NUmber,perdioendo precision y su tipo de dato es :
- ${typeof(numeroGRANde4)}`);
-
- numeroGRANde3=BigInt(1234567890123456789012345678901234567890);
- console.log(`El cuarto experimento de un numero grande es ${numeroGRANde3},que NO es soportado por NUmber,perdioendo precision y su tipo de dato es :
- ${typeof(numeroGRANde3)}`);
-
- let numeroGrande3=123456789012345678901234567890;
-let numeroGrande4=1234567890123456789012345678901234567890
-
-console.log(`El primer experimento de un número grande es: ${numeroGrande}, que si es soportado por NUMBER, y su tipo de dato es: ${typeof(numeroGrande)}`);
-console.log(`El segundo experimento de un número grande es: ${numeroGrande2}, que si es soportado por NUMBER, y su tipo de dato es: ${typeof(numeroGrande2)}`);
-console.log(`El tercer experimento de un número grande es: ${numeroGrande3}, que no es soportado por NUMBER, perdiendo precisión y su tipo de dato es: ${typeof(numeroGrande3)}`);
-console.log(`El cuarto experimento de un número grande es: ${numeroGrande4}, que si es soportado por NUMBER, perdiendo precisión y su tipo de dato es: ${typeof(numeroGrande4)}`);
+//Manipulando los STRINGS
+console.log(`Más adelante podremos transformar el contenido de los STRINGS usando los métodos y funciones especificas, como convertir su valor a mayúsculas: ${alumno}=>${alumno.toUpperCase()}`);
+console.log(`O en su defecto a minusculas: ${producto}=>${producto.toLowerCase()}`);
 
 
-numeroGrande3=BigInt("123456789012345678901234567890")
-console.log(`El quinto experimento de un número grande es: ${numeroGrande3}, que ya fue convertido a BIGINT, y su tipo de dato es: ${typeof(numeroGrande3)}`);
+//5. BIGINT (Número de MAyor Amplitud)
 
-numeroGrande4=BigInt("1234567890123456789012345678901234567890")
-console.log(`El sexto experimento de un número grande es: ${numeroGrande4}, que ya fue convertido a BIGINT, y su tipo de dato es: ${typeof(numeroGrande4)}`);
+console.warn("---Tipo de Dato - BIGINT (Número Amplio)");
+const numeroGrande = 1234567890;
+const numeroGrande2 = 12345678901234567890;
+let numeroGrande3 = 123456789012345678901234567890;
+let numeroGrande4 = 1234567890123456789012345678901234567890;
+console.log(`El primer experimento de un número grande es: ${numeroGrande}, que si es soportado por NUMBER,  y su tipo de dato es ${typeof (numeroGrande)}`);
+console.log(`El segundo experimento de un número grande es: ${numeroGrande2}, que si es soportado por NUMBER, y su tipo de dato es ${typeof (numeroGrande2)}`);
+console.log(`El tercero experimento de un número grande es: ${numeroGrande3}, que NO es soportado por NUMBER, perdiendo presición y su tipo de dato es ${typeof (numeroGrande3)}`);
+console.log(`El cuarto experimento de un número grande es: ${numeroGrande4}, que NO es soportado por NUMBER, perdiendo presición y su tipo de dato es ${typeof (numeroGrande4)}`);
+
+numeroGrande3 = BigInt(123456789012345678901234567890);
+numeroGrande4 = BigInt(1234567890123456789012345678901234567890);
+
+console.log(`El quinto experimento de un número grande es: ${numeroGrande3}, que ya fue convertido a BIGINT, y su tipo de dato es ${typeof (numeroGrande3)}`);
+console.log(`El sexto experimento de un número grande es: ${numeroGrande4}, que ya fue convertido a BIGINT, y su tipo de dato es ${typeof (numeroGrande4)}`);
+
+//¿Se pueden realizar operaciones matematicas entre un number y un bigint?
+const numero = 129;
+console.log(`Intetando realizar la suma de: numer+numeroGrande3 el resultado es ${BigInt(numero) + numeroGrande3}`);
+
+//6. SYMBOL (Símbolo)
+console.warn("---Tipo de Dato - SYMBOL (Símbolo o único)");
+const numero1 = 5;
+const numero2 = 5.0;
+const numero3 = "5";
+const numero4 = "5.0";
+const numero5 = Symbol(5);
+const numero6 = Symbol(5);
+const numero7 = Symbol(5.0);
+const numero8 = Symbol("5");
+const numero9 = Symbol("5.0");
+//Pruebas comparativas
+console.log("¿Es igual 5 y 5.0?")
+if (numero1 == numero2) {
+    console.log(`Se comparó numero1 con numero2, determinando que tienen el mismo valor.`);
+} else {
+    console.log(`Se comparó numero1 con numero2, detrerminando que NO tienen el mismo valor.`);
+}
+//numero3
+
+console.log("¿Es igual 5 y \"5\"")
+if (numero1 == numero3) {
+    console.log(`Se comparó numero1 con numero3, determinando que tienen el mismo valor.`);
+} else {
+    console.log(`Se comparó numero1 con numero3, detrerminando que NO tienen el mismo valor.`);
+}
+
+console.log("¿Es igual 5 y \"5.0\"")
+if (numero1 === numero3) {
+    console.log(`Se comparó numero1 con numero3, determinando que tienen el mismo tipo de DATO.`);
+} else {
+    console.log(`Se comparó numero1 con numero3, detrerminando que NO tienen el mismo tipo de DATO.`);
+}
+//numero4
+console.log("¿Es igual 5 y \"5.0\"")
+if (numero1 === numero4) {
+    console.log(`Se comparó numero1 con numero4, determinando que tienen el mismo tipo de DATO.`);
+} else {
+    console.log(`Se comparó numero1 con numero2, detrerminando que NO tienen el mismo tipo de DATO.`);
+}
+//numero5
+console.log(`¿Es igual 5 y Symbol(5)?, antes de compararlos analizamos que tipo de dato son: siendo numero1: ${typeof (numero1)} y numero5: ${typeof (numero5)}`);
+if (numero1 === numero5) {
+    console.log(`Se comparó numero1 con numero5, determinando que tienen el mismo tipo de DATO.`);
+} else {
+    console.log(`Se comparó numero1 con numero5, detrerminando que NO tienen el mismo tipo de DATO.`);
+}
+//numero 6 
+console.log(`¿Es igual 5 y Symbol(5)?,Antes de compararlo analizamos que tipo de dato son siendo numero1 : ${typeof (numero1)}y numero 5 ${typeof (numero5)}
+`);
+if (numero1 == numero5) {
+    console.log(`Se compro numero1 con numero5, determinar que tienenel mismo dato.`);
+} else {
+    console.log(`Se comparo numero1 con numero 5 determinando que No tienen el mismo tipo de dato `)
+}
+
+//7.NULL
+console.warn("---Tipo de dato - Null (Nulo o vacio )");
+let nombreUsuario = null;
+let passUsuario = null;
+let generoUsuario = null;
+let estatusRelacionalesSentimientos = null;
+let Fecha_ultimoPost;
+//Supongamos que estamos programando una red social , tipo Facebook en la que parte de informcacion se publicara en el perfil del usuario.
+
+//Si el usuario no ha inicializado sesion en el dispositivo movil o en la aplicacion web, puede explorar contenido de acceso publico y no existira informacion para mostrar 
+
+//En la HU (Historia de usuario), que el usuario desea logearse deberas ingresar su numero telefonico o correo electronico y una contraseña que deberemos guardar en las variables previamente declaradas.
+
+//Supongamos que el usuario :Marco R. con correo electronico marco.ramgmail.com desea ingresar con su contraseña : pas1234
+nombreUsuario = "marco.ram@gmail.com";
+passUsuario = "pass123";
+
+//En este momento de ejecucion del sistema no debemos sus genero , ni su estatus de la relacion sentimental .
+console.log(`El usuario : ${nombreUsuario}esta intento logearse con una contraseña de :${passUsuario
+    }`)
+
+//lo que prosigue  es que el sistema contejera los datos ingresados con la base datos y en caso de que los datos sean correctos comenzara0 la sesion  en la plataforma actualizando estos assertValidExecutionArguments.
+
+//Dado que Marco es el genero
+
+
